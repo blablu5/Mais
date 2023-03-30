e0f5157b53400f955243bcff91783cb96bf64f5e
blablu5 <johannareber05@gmail.com>
2023-03-30 12:52:48 +0200

    Job 1

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,76 @@
 <script setup>
-
+import job1 from './components/job1.vue';
+import Job2 from './components/job2.vue';
 </script>
 
 <script>
 export default {
+  components :{
+    job1,
+    Job2
+},
   data(){
     return{
       entschieden: false,
       beitreten: true,
-      maisinfo: "Mais ist reich an Ballaststoffen (10 Gramm pro 100 Gramm): Dies sind unverdauliche Bestandteile der Nahrung, die die Verdauung fördern. Außerdem eignet sich Mais mit mehr als 8 Gramm Eiweiß gut als pflanzliche Proteinquelle. Bei den Mineralstoffen sind vor allem Kalium, Eisen und Magnesium in nennenswerten Mengen vorhanden und tragen zu einer gesunden Ernährung bei. "
+      erklärungMais: "Deine Aufgabe ist es einen Flyer für den Mais-Fanclub zu erstellen. Hier hast du sechs Artikel. Entscheide dich welche Artikel du dem Flyer hinzufügen möchtest und welche nicht.",
+      erklärungZucchetti: "Deine Aufgabe ist es einen Flyer für den Zucchetti-Fanclub zu erstellen. Hier hast du sechs Artikel. Entscheide dich welche Artikel du dem Flyer hinzufügen möchtest und welche nicht.",
+      maisinfo: "Teste wie gut du Personen überzeugen kannst deinem Fanclub beizutreten. Entscheide dich zunächst, ob du dich Mais oder Zucchetti anschliessen möchtest.",
+      disabled: false
     }
   },
   methods: {
     change(beitreten) {
-      this.beitreten = beitreten,
-      this.entschieden = true
+      this.beitreten = beitreten
+      if (!this.entschieden) {
+        this.entschieden = true 
+        this.scrolling()
+      }
+      this.disabled = true
+    },
+    scrolling() {
+      this.$refs["arrows"].scrollIntoView({ behavior: "smooth" })
     }
   }
 }
+
 </script>
+
 <template>  
-    <div class="bg-black">
+    <div class="bg-black min-h-screen">
         <header class="py-5 bg-greenbg bg-bottom pb-8">
-            <h1 class="text-white font-bold text-5xl text-center font-fantasy">Mais Fanclub</h1>
+            <h1 class="text-white font-bold text-5xl text-center font-fantasy">Mais-Fanclub</h1>
         </header>
         <div class="pb-5 bg-yellowbg bg-bottom">
-          <div class="w-96 pb-5 mx-auto">
-              <h2 class="font-fantasy font-bold text-4xl">Mais Info:</h2>
+          <div class="mx-20 pb-5">
+              <h2 class="font-fantasy font-bold text-4xl">Treniere Personen von deinem Fanclub zu überzeugen.</h2>
               <p class="font-fantasy font-bold">{{maisinfo}}</p>
           </div>    
         </div>
         <div>
             <div class="mx-auto w-96 rounded-full bg-yellow-500">
-                <h2 class="font-fantasy text-center">Möchtest du dem Mais Fanclub beitreten?</h2>
+                <h2 class="font-fantasy text-center">Für welchen Fanclub möchtest du arbeiten?</h2>
             </div>
             <div class="flex justify-center">
-                <button v-on:click="change(true)" class="mr-10 mt-5 w-32 h-32 rounded-full bg-yellow-400 active:bg-yellow-900" type="button">
-                    <h2 class="active:text-white text-5xl align-middle active:align-top">Ja</h2>
+                <button :disabled="disabled" v-on:click="change(true)" class="mr-10 mt-5 w-32 h-32 rounded-full bg-yellow-400 active:bg-yellow-900" type="button">
+                    <h2 class="active:text-white text-2xl align-middle active:align-top font-fantasy font-bold">Mais</h2>
                 </button>
-                <button v-on:click="change(false)" class="ml-10 mt-5 w-32 h-32 rounded-full bg-green-900 active:bg-green-600" type="button">
-                    <h2 class="text-white active:text-black text-5xl align-middle active:align-top">Nein</h2>
+                <button :disabled="disabled" v-on:click="change(false)" class="ml-10 mt-5 w-32 h-32 rounded-full bg-green-900 active:bg-green-600" type="button">
+                    <h2 class="text-white active:text-black text-2xl align-middle active:align-top font-fantasy font-bold">Zucchetti</h2>
                 </button>
             </div>
         </div>
-        <div class="flex justify-center">
+        <div ref="arrows" class="flex justify-center">
           <img class="pr-20" v-if="beitreten&&entschieden" src="./assets/arrowYes.svg" alt="Pfeil">
           <img class="pl-20" v-else-if="!beitreten" src="./assets/arrowNo.svg" alt="Pfeil">
         </div>
+        <div v-if="beitreten&&entschieden">
+          <p class="text-white mx-5 my-10">{{ erklärungMais }}</p>
+          <job1/>
+        </div>
+        <div v-else-if="!beitreten">
+          <p class="text-white mx-5 my-10">{{ erklärungZucchetti }}</p>
+          <job1/>
+        </div>
     </div>
-    
 </template>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Mais/',
   plugins: [vue()],
   resolve: {
     alias: {
